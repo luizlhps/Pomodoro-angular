a222b9d1bd0c8688a3a92c15efccf08eac9cc3bf
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2024-02-22 00:37:09 -0300

    feat implements countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,56 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'main';
+  buttonText: string = 'Iniciar';
+  twentyFiveMinutesInSeconds: number = 25 * 60;
+  seconds: number = this.twentyFiveMinutesInSeconds;
+  formattedSeconds: string = this.formatTime(this.seconds);
+  pomodoroStart: boolean = false;
+  interval: NodeJS.Timeout | undefined;
+
+  constructor() {}
+
+  private formatTime(seconds: number): string {
+    const minutes: number = Math.floor(seconds / 60);
+    const remainingSeconds: number = seconds % 60;
+    const formattedMinutes: string =
+      minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const formattedSeconds: string =
+      remainingSeconds < 10 ? `0${remainingSeconds}` : `${remainingSeconds}`;
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+
+  private updateFormattedSeconds() {
+    this.formattedSeconds = this.formatTime(this.seconds);
+  }
+
+  startTimeDecrease() {
+    if (this.buttonText === 'Pausar') {
+      console.log(this.interval);
+      if (this.interval) {
+        clearInterval(this.interval);
+      }
+
+      this.buttonText = 'Iniciar';
+      return;
+    }
+
+    this.pomodoroStart = true;
+
+    this.interval = setInterval(() => {
+      this.seconds--;
+
+      if (this.seconds < 0) {
+        clearInterval(this.interval);
+        this.seconds = 0;
+        this.buttonText = 'Iniciar';
+        console.log('Timer finalizado!');
+        return;
+      }
+
+      this.updateFormattedSeconds();
+    }, 1000);
+
+    this.buttonText = 'Pausar';
+  }
 }
